3f51f589d19f1209eeddc00d1726f13d3b91d809
nadarian3 <nadarian3@gmail.com>
2026-01-05 14:50:30 +0100

    Update Magazine.java

--- Projet_POO/src/main/java/com/bibliotheque/model/Magazine.java
+++ Projet_POO/src/main/java/com/bibliotheque/model/Magazine.java
@@ -1,22 +1,11 @@
 package com.bibliotheque.model;
 
-/**
- * Classe représentant un magazine dans la bibliothèque.
- */
+
 public class Magazine extends Document implements Empruntable {
     private int numero;
     private String mois;
     private boolean disponible;
 
-    /**
-     * Constructeur d'un magazine.
-     *
-     * @param id            l'identifiant du magazine
-     * @param titre         le titre du magazine
-     * @param numero        le numéro du magazine
-     * @param mois          le mois de publication
-     * @param disponible    la disponibilité du magazine
-     */
     public Magazine(String id, String titre, int numero, String mois, boolean disponible) {
         super(id, titre);
         this.numero = numero;
@@ -24,12 +13,6 @@ public class Magazine extends Document implements Empruntable {
         this.disponible = disponible;
     }
 
-    /**
-     * Calcule la pénalité de retard pour un magazine.
-     * Pénalité : 1 DH par jour de retard.
-     *
-     * @return la pénalité en DH
-     */
     @Override
     public double calculerPenaliteRetard() {
         return 1.0; // 1 DH par jour
@@ -87,3 +70,4 @@ public class Magazine extends Document implements Empruntable {
                 '}';
     }
 }
+
