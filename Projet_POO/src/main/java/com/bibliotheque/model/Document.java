4fd4aff8ef7ba7e1a1e183ef99860ea4c2ddee96
nadarian3 <nadarian3@gmail.com>
2026-01-05 14:45:39 +0100

    Update Document.java

--- Projet_POO/src/main/java/com/bibliotheque/model/Document.java
+++ Projet_POO/src/main/java/com/bibliotheque/model/Document.java
@@ -1,30 +1,14 @@
 package com.bibliotheque.model;
-
-/**
- * Classe abstraite représentant un document dans la bibliothèque.
- * Tous les documents doivent calculer une pénalité de retard.
- */
 public abstract class Document {
     protected String id;
     protected String titre;
 
-    /**
-     * Constructeur du document.
-     *
-     * @param id    l'identifiant unique du document
-     * @param titre le titre du document
-     */
+
     public Document(String id, String titre) {
         this.id = id;
         this.titre = titre;
     }
 
-    /**
-     * Calcule la pénalité en cas de retard.
-     * À implémenter par les classes concrètes.
-     *
-     * @return la pénalité en DH
-     */
     public abstract double calculerPenaliteRetard();
 
     // Getters et Setters
@@ -45,3 +29,4 @@ public abstract class Document {
         this.titre = titre;
     }
 }
+
