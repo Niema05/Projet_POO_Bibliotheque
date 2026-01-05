516dc903ec09f78ae35d6c23859365e3f9c527d3
nadarian3 <nadarian3@gmail.com>
2026-01-05 14:32:56 +0100

    Refactor LivreDAO interface documentation

--- Projet_POO/src/main/java/com/bibliotheque/dao/LivreDAO.java
+++ Projet_POO/src/main/java/com/bibliotheque/dao/LivreDAO.java
@@ -8,19 +8,11 @@ import java.util.List;
  * Interface DAO pour les livres.
  */
 public interface LivreDAO extends DAO<Livre> {
-    /**
-     * Recherche des livres par auteur.
-     *
-     * @param auteur l'auteur à rechercher
-     * @return une liste de livres de l'auteur
-     * @throws SQLException si une erreur SQL survient
-     */
+  
     List<Livre> findByAuteur(String auteur) throws SQLException;
 
     /**
      * Recherche des livres par titre.
-     *
-     * @param titre le titre à rechercher
      * @return une liste de livres correspondant au titre
      * @throws SQLException si une erreur SQL survient
      */
@@ -36,19 +28,18 @@ public interface LivreDAO extends DAO<Livre> {
 
     /**
      * Recherche un livre par ISBN.
-     *
-     * @param isbn l'ISBN du livre
      * @return le livre trouvé, null sinon
      * @throws SQLException si une erreur SQL survient
      */
     Livre findByISBN(String isbn) throws SQLException;
 
-    /**
-     * Vérifie si un livre avec cet ISBN existe déjà.
-     *
-     * @param isbn l'ISBN à vérifier
+       /**
+       Vérifie si un livre avec cet ISBN existe déjà.
+     
+
      * @return true si le livre existe, false sinon
      * @throws SQLException si une erreur SQL survient
      */
     boolean existsByISBN(String isbn) throws SQLException;
 }
+
