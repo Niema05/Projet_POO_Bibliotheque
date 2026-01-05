2b117f055cddfe460ad7a47deac82860aebf31d8
SalmaHilali <salmahilali3042005@gmail.com>
2026-01-05 14:38:44 +0100

    Refactor EmpruntDAO interface comments

--- Projet_POO/src/main/java/com/bibliotheque/dao/EmpruntDAO.java
+++ Projet_POO/src/main/java/com/bibliotheque/dao/EmpruntDAO.java
@@ -4,41 +4,23 @@ import com.bibliotheque.model.Emprunt;
 import java.sql.SQLException;
 import java.util.List;
 
-/**
- * Interface DAO pour les emprunts.
- */
+// Interface DAO pour les emprunts.
+ 
 public interface EmpruntDAO extends DAO<Emprunt> {
-    /**
-     * Récupère tous les emprunts d'un membre.
-     *
-     * @param membreId l'identifiant du membre
-     * @return une liste des emprunts du membre
-     * @throws SQLException si une erreur SQL survient
-     */
+    // Récupère tous les emprunts d'un membre.
+     
     List<Emprunt> findByMembre(int membreId) throws SQLException;
 
-    /**
-     * Récupère tous les emprunts en cours (non retournés).
-     *
-     * @return une liste des emprunts en cours
-     * @throws SQLException si une erreur SQL survient
-     */
+    // Récupère tous les emprunts en cours (non retournés).
+     
     List<Emprunt> findEnCours() throws SQLException;
 
-    /**
-     * Récupère tous les emprunts en retard.
-     *
-     * @return une liste des emprunts en retard
-     * @throws SQLException si une erreur SQL survient
-     */
+    // Récupère tous les emprunts en retard.
+     
     List<Emprunt> findEnRetard() throws SQLException;
 
-    /**
-     * Compte le nombre d'emprunts en cours pour un membre.
-     *
-     * @param membreId l'identifiant du membre
-     * @return le nombre d'emprunts en cours
-     * @throws SQLException si une erreur SQL survient
-     */
+    // Compte le nombre d'emprunts en cours pour un membre.
+     
     int countEmpruntsEnCours(int membreId) throws SQLException;
 }
+
